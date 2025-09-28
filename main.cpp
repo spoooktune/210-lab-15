8e243389467a0d906e4e6287791f54e77015fd33
spoooktune <asriprasana5@gmail.com>
2025-09-27 23:44:38 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Movie{
+    // class members
+};
+
+int main(){
+    // create vector of Movie objects
+}

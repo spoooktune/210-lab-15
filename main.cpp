cb6863f6259b8abfeace95cd20adfc3d28255f46
spoooktune <asriprasana5@gmail.com>
2025-09-28 00:09:47 -0700

    Added code to main: takes input from file, create + populates object, places object into vector

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+const int NUM_MOVIES = 4;
+
+// Movie Class: 3 vars, 3 getters, 3 setters, 1 display method
 class Movie{
     private:
         string title;
@@ -20,20 +24,39 @@ class Movie{
         void print();
 };
 
+// Display method
 void Movie::print(){
     cout << "Movie: " << title << endl;
     cout << "> Year Released: " << releaseYear << endl;
     cout << "> Screenwriter: " << screenwriter << endl << endl;
 }
 
+// Takes input from input file, puts into temp Movie obj, puts obj into vector
 int main(){
     vector<Movie> movieList;
     ifstream in;
     in.open("input.txt");
     if (in.good()){
-        // get inputs
+        string t, sw;
+        int ry;
+        for (int i = 0; i < NUM_MOVIES; i++){
+            Movie temp;
+            getline(in, t);
+            in.ignore();
+            in >> ry;
+            getline(in, sw);
+            in.ignore();
+            temp.setTitle(t);
+            temp.setReleaseYear(ry);
+            temp.setScreenwriter(sw);
+            movieList.push_back(temp);
+        }
+        in.close();
     }
     else{
         cout << "File not found" << endl;
+    }
+    for (int i = 0; i < NUM_MOVIES; i++){
+        movieList[i].print();
     }
 }

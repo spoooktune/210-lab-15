019efa1d3fd5a75d0e7b6cbd84ae77d7a97877b1
spoooktune <asriprasana5@gmail.com>
2025-09-27 23:56:55 -0700

    Created class Movie, added member vars and functions, added code to get file input

--- main.cpp
+++ main.cpp
@@ -5,9 +5,35 @@
 using namespace std;
 
 class Movie{
-    // class members
+    private:
+        string title;
+        string screenwriter;
+        int releaseYear;
+
+    public:
+        string getTitle() {return title;}
+        string getScreenwriter() {return screenwriter;}
+        int getReleaseYear() {return releaseYear;}
+        void setTitle(string t) {title = t;}
+        void setScreenwriter(string sw) {screenwriter = sw;}
+        void setReleaseYear(int ry) {releaseYear = ry;}
+        void print();
 };
 
+void Movie::print(){
+    cout << "Movie: " << title << endl;
+    cout << "> Year Released: " << releaseYear << endl;
+    cout << "> Screenwriter: " << screenwriter << endl << endl;
+}
+
 int main(){
-    // create vector of Movie objects
+    vector<Movie> movieList;
+    ifstream in;
+    in.open("input.txt");
+    if (in.good()){
+        // get inputs
+    }
+    else{
+        cout << "File not found" << endl;
+    }
 }
